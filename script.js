2ced6a5cc19ec69ff5626e2d749270d75a0760c2
RICHA SINGH <91095253+richasingh-92@users.noreply.github.com>
2022-12-30 11:34:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+alert('HELLO (๑˙❥˙๑) \n \nAll Suggestions are welcomed \n \nTHANKS for visit  👉')
+
+
+$(document).ready(function(){
+    $(".vibrate").click(function(){
+        window.navigator.vibrate(50);
+     
+    });
+});
